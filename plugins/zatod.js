35b6870c337ae28b70899c1ab17bc2d5171b8e2c
Aminwsf <154097506+Aminwsf@users.noreply.github.com>
2024-06-30 21:10:02 +0100

    Update zatod.js

--- plugins/zatod.js
+++ plugins/zatod.js
@@ -3,7 +3,7 @@ let handler = m => m
 handler.before = async (m, { conn, args }) => {
   
   if (m.isBaileys || (m.isBaileys && m.fromMe && m.isGroup)) return
-  if (m.isGroup) return 
+  if (m.isGroup || m.fromMe) return
   if (m.chat.endsWith('broadcast')) return
   
   await delay(3000)
