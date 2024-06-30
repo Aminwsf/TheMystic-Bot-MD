77c8608c8becaea275810ba532b0605719da0bf3
Aminwsf <154097506+Aminwsf@users.noreply.github.com>
2024-06-30 21:14:24 +0100

    Create _01.js

---
+++ plugins/_01.js
@@ -0,0 +1,17 @@
+let handler = m => m
+
+handler.before = async (m, { conn, args }) => {
+  
+  if (m.isBaileys || (m.isBaileys && m.fromMe && m.isGroup)) return
+  if (m.isGroup || m.fromMe) return
+  if (m.chat.endsWith('broadcast')) return
+  
+  await delay(3000)
+ 
+}
+
+export default handler
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
