9c9b9d46c2a8a66155f6bd181b6f2df82a6ed99e
Aminwsf <154097506+Aminwsf@users.noreply.github.com>
2024-07-03 18:05:11 +0100

    Update _delete.js

--- plugins/_delete.js
+++ plugins/_delete.js
@@ -1,6 +1,14 @@
+import { getLastMessageInChat } from '@whiskeysockets/baileys'
+
 const handler = async (m, {conn, text}) => {
+
+	const lastMsgInChat = await getLastMessageInChat(m.chat) // implement this on your end
+await sock.chatModify({
+  delete: true,
+  lastMessages: [{ key: lastMsgInChat.key, messageTimestamp: lastMsgInChat.messageTimestamp }]
+},
+m.chat)
 	
-await conn.chatModify({delete: true}, m.chat)
 m.reply("done")
 };
 handler.help = ['afk [alasan]'];
